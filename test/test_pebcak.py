71dfc0ba0a07474fcb471e9c075d2af5f1321149
matthew.gill <matthew.gill@servicenow.com>
2020-12-04 13:46:17 -0800

    #4 improve warnings

--- test/test_pebcak.py
+++ test/test_pebcak.py
@@ -11,18 +11,25 @@ class TestPEBCAK(TestCase):
     def test_table(self):
         client = ServiceNowClient(self.c.server, self.c.credentials)
         gr = client.GlideRecord('sys_user_list')
+        client.session.close()
         self.assertRaises(RequestException, gr.get, 'doesntmatter')
 
     def test_instance(self):
         with self.assertRaises(InstanceException) as context:
-            client = ServiceNowClient('test.x', self.c.credentials)
+            try:
+                client = ServiceNowClient('test.x', self.c.credentials)
+            finally:
+                client.session.close()
         self.assertTrue(isinstance(context.exception, InstanceException))
 
     def test_creds(self):
         with self.assertRaises(AuthenticationException) as context:
             client = ServiceNowClient(self.c.server, ('test','test'))
-            gr = client.GlideRecord('sys_user')
-            gr.get('asdf');
+            try:
+                gr = client.GlideRecord('sys_user')
+                gr.get('asdf');
+            finally:
+                client.session.close()
         self.assertTrue(isinstance(context.exception, AuthenticationException))
 
     def test_no_result_without_query(self):
@@ -32,4 +39,5 @@ class TestPEBCAK(TestCase):
         self.assertFalse(gr.has_next())
         for e in gr:
             assert "Should not have iterated!"
+        client.session.close()
 
