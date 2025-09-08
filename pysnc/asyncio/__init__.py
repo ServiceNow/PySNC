4b6aad53dbeb8b287fea102da82f9f8dba849862
matthew.gill <matthew.gill@servicenow.com>
2025-09-08 09:11:47 -0700

    feat: seeing if this works

--- pysnc/asyncio/__init__.py
+++ pysnc/asyncio/__init__.py
@@ -2,10 +2,14 @@
 Asynchronous implementation of the pysnc package using httpx.AsyncClient.
 """
 
-from .client import AsyncTableAPI, AsyncBatchAPI, AsyncAttachmentAPI, AsyncServiceNowClient
-from .record import AsyncGlideRecord
-from .attachment import AsyncAttachment
-from .auth import AsyncServiceNowFlow, AsyncServiceNowPasswordGrantFlow, AsyncServiceNowJWTAuth
+try:
+    from .client import AsyncTableAPI, AsyncBatchAPI, AsyncAttachmentAPI, AsyncServiceNowClient
+    from .record import AsyncGlideRecord
+    from .attachment import AsyncAttachment
+    from .auth import AsyncServiceNowFlow, AsyncServiceNowPasswordGrantFlow, AsyncServiceNowJWTAuth
+except ImportError:
+    raise ImportError("httpx is required for the asyncio module. Please install pysnc with the 'asyncio' extra.")
+
 
 __all__ = [
     'AsyncServiceNowClient',
